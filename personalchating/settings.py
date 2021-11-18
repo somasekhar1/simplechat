0ec91da6daeffd7c391e2a503f66b30aba3a9c47
somasekhar1 <somasekhar0023@gmail.com>
2021-11-18 12:43:46 +0530

    host

--- personalchating/settings.py
+++ personalchating/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+fdlbt1kxh^p2%%jdd5-tzuh(ud)n=8i32l2t8bth(k_at#gk@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simplechatings.herokuapp.com']
 
 
 # Application definition
